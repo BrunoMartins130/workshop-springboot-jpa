62ec3a78fa2c45a362ce224d14ce94c1ffbb6b6a
BrunoMartins130 <brunomartinsoficial96@hotmail.com>
2023-01-18 15:20:40 -0300

    H2 database, test profile, JPA

--- src/main/java/springBoot/curso/entitites/User.java
+++ src/main/java/springBoot/curso/entitites/User.java
@@ -1,20 +1,24 @@
 package springBoot.curso.entitites;
 
-import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User(){
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
