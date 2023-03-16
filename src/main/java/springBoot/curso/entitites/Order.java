b9dbb77abf48e95ea548a1b742069237b29fc4c8
BrunoMartins130 <brunomartinsoficial96@hotmail.com>
2023-03-15 21:50:17 -0300

    Payment, one-to-one association & Subtotal & Total methods

--- src/main/java/springBoot/curso/entitites/Order.java
+++ src/main/java/springBoot/curso/entitites/Order.java
@@ -29,6 +29,8 @@ public class Order implements Serializable{
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
 
@@ -73,9 +75,25 @@ public class Order implements Serializable{
     public void setClient(User client) {
         this.client = client;
     }
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
     @Override
     public boolean equals(Object o) {
