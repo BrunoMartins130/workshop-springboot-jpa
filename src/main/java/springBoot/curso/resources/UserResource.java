0f6c3de1ab270d34dcbfc43495d0ea480e49af7d
BrunoMartins130 <brunomartinsoficial96@hotmail.com>
2023-03-20 20:10:28 -0300

    User insert

--- src/main/java/springBoot/curso/resources/UserResource.java
+++ src/main/java/springBoot/curso/resources/UserResource.java
@@ -2,13 +2,12 @@ package springBoot.curso.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import springBoot.curso.entitites.User;
 import springBoot.curso.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +27,11 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
 
     }
-
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+       obj = service.insert(obj);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+       return ResponseEntity.created(uri).body(obj);
+    }
 
 }
